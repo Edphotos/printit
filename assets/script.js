8a07be1957824a08d88427125dfa206c56fa2024
Edouard Ott <edouardott@Edouards-MacBook-Pro.local>
2024-11-06 13:35:44 +0100

    integration du carroussel

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,53 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let index=0
+showslide()
+const arrow_left = document.querySelector(".arrow_left")
+arrow_left.addEventListener("click", function(){
+	if(index===0){
+		index=3
+	 } else {
+		index--
+	 }
+
+	 showslide()
+	 dots()
+})
+const arrow_right = document.querySelector(".arrow_right")
+arrow_right.addEventListener("click", function(){
+if(index===3){
+	index=0
+ } else {
+	index++
+ }
+showslide()
+dots()
+})
+function showslide(){
+	const curentslide=slides[index]
+	const bannerimg=document.querySelector(".banner-img")
+	bannerimg.src="./assets/images/slideshow/"+curentslide.image
+	const bannertext=document.querySelector("#banner p")
+	bannertext.innerHTML=curentslide.tagLine
+	
+}
+function dots(){
+	const dots=document.querySelector(".dots")
+	dots.innerHTML=""
+	let compteur=0
+	for (const slide of slides){
+		const dot=document.createElement("div")
+		dot.classList.add("dot")
+		
+		if (index===compteur){
+			dot.classList.add("dot_selected")
+			
+		}
+		dots.append(dot)
+		compteur++
+	}
+}
+dots()
+
